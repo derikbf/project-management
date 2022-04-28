16f09ecd495f35bfc43c4e5a23f11a77bee68a3d
derikbf <derik.favoretti@vilaapps.com.br>
2022-04-27 22:28:44 -0300

    Atualizando o Readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: "user@user.com",
+  password: "User123", 
+  password_confirmation: "User123"
+)
+
+puts "##########################"
+  puts "USER DEFAULT"
+  puts "Email: user@user.com"
+  puts "Password: User123"
+puts "##########################"
+
+Project.create!([
+  { name: "Project Number One", description: "Project", user_id: 1 },
+  { name: "Project Number Two", description: "Project", user_id: 1 },
+  { name: "Project Number Three", description: "Project", user_id: 1 }
+])
+
+puts "PROJECTS CREATED"
+puts "##########################"
+
+30.times do |i|
+  Task.create!(
+    name: "Task number 10.000.00#{i}",
+    description: "Description number 15.000.00#{i}",
+    status: ['not-started', 'in-progress', 'complete'].sample,
+    project_id: rand(1..3)
+  )
+end
+
+puts "TASKS CREATED"
+puts "##########################"
